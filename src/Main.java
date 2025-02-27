a14a414b10ea0214faf7d337762f03d327efe8bd
YuriSilvaCrvl <yurisilvacrvl@gmail.com>
2025-02-27 18:21:21 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Verificar se o número está entre 5 e 20.
+        //Verificar se o número é menor que 5 ou maior que 10
         Scanner leitor = new Scanner(System.in);
 
         System.out.print("Digite algo:");
-        int a = leitor.nextInt();
+        float a = leitor.nextFloat();
 
-        if(a >= 5 && a <=20){
-            System.out.println("O número "+a+" se encaixa nas especificações de estar entre 5 e 20.");
+        if(a < 5 || a > 10){
+            System.out.println("O número "+a+" se encaixa nas especificações de ser menor que 5 ou maior que 10");
         }else{
-            System.out.println(a+"  <-- este número não está entre 5 e 20.");
+            System.out.println(a+"  <-- este número não é menor que 5 e nem maior que 10.");
         }
     }
 }
