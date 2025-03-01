a1dc222fd78514a39b8b9e4d696e81963e6d4fb1
YuriSilvaCrvl <yurisilvacrvl@gmail.com>
2025-02-28 22:42:47 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Verificar se o número é menor que 5 ou maior que 10
+        //Utilizando o Switch Case.
+        System.out.println("   Escolha o dia da semana:   ");
+        System.out.println("1 - Domingo");
+        System.out.println("2 - Segunda");
+        System.out.println("3 - Terça");
+        System.out.println("4 - Quarta");
+        System.out.println("5 - Quinta");
+        System.out.println("6 - Sexta");
+        System.out.println("7 - Sábado");
+
         Scanner leitor = new Scanner(System.in);
+        int opcao = leitor.nextInt();
 
-        System.out.print("Digite algo:");
-        float a = leitor.nextFloat();
-
-        if(a < 5 || a > 10){
-            System.out.println("O número "+a+" se encaixa nas especificações de ser menor que 5 ou maior que 10");
-        }else{
-            System.out.println(a+"  <-- este número não é menor que 5 e nem maior que 10.");
+        switch (opcao) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
         }
     }
 }
